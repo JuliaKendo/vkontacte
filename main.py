19df83a85c2467960b9660ba4038214f16232477
Julia Kendo <ya1814@yandex.ru>
2020-04-26 23:08:30 +0300

    Revision of the code
    Revision of the code 26042020

--- main.py
+++ main.py
@@ -2,32 +2,25 @@ import requests
 import os
 from dotenv import load_dotenv
 import logging
-import time
 import datetime
 import plotly.graph_objects as go
 
-load_dotenv()
-logging.basicConfig(level = logging.DEBUG, filename = u'log.txt')
-
 VERSION_VKONTACTE = '5.52'
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+VK_SERVICE_KEY = None
 PERIOD_OF_DAYS_ANALYSIS = 7
 
-def get_url(url_method, params=None):
-    url = f'https://api.vk.com/method/{url_method}'
+def get_url(api_method):
+    url = f'https://api.vk.com/method/{api_method}'
     return url
 
-def query_to_site(Url, Params, Headers=None):
+def get_data_from_site(url, params, headers=None):
     try:
-        if Headers:
-            responce = requests.get(Url, headers=Headers, params=Params)
-        else:
-            responce = requests.get(Url, params=Params)
+        response = requests.get(url, headers=headers or {}, params=params)
     except requests.exceptions.HTTPError as error:
-        logging.error(u'Ошибка получения данных по ссылке {0}:\n{1}'.format(Url, error))
-        return {}
-    responce.raise_for_status()
-    return responce.json()
+        logging.error(u'Ошибка получения данных по ссылке {0}:\n{1}'.format(url, error))
+        return
+    response.raise_for_status()
+    return response.json()
 
 def get_table_of_periods():
     list_of_periods = []
@@ -40,9 +33,9 @@ def get_table_of_periods():
 
     return list_of_periods
 
-def get_occurence():
-    occurence = [[],[]]
-    params = {'v':VERSION_VKONTACTE,'access_token':ACCESS_TOKEN}
+def get_occurrences():
+    occurrences = [[],[]]
+    params = {'v':VERSION_VKONTACTE,'access_token':VK_SERVICE_KEY}
     params['q'] = 'Coca-Cola'
 
     list_of_periods = get_table_of_periods()
@@ -54,25 +47,27 @@ def get_occurence():
         params['start_time'] = period[1]
         params['end_time'] = list_of_periods[i][1]
         while True:
-            json_info = query_to_site(url, params)
-            time.sleep(1)
-            next_form = json_info['response'].get('next_from')
-            if not next_form:
+            data_from_site = get_data_from_site(url, params)
+            if data_from_site:
+                next_form = data_from_site['response'].get('next_from')
+            if next_form is None:
                 break
-            for item in json_info['response']['items']:
-                num_of_posts += 1
-
+            num_of_posts += len(data_from_site['response']['items'])
             params['start_from'] = next_form
-        occurence[0].append(end_time.strftime('%d.%m.%Y'))
-        occurence[1].append(num_of_posts)
-
-    return occurence
+            
+        occurrences[0].append(end_time.strftime('%d.%m.%Y'))
+        occurrences[1].append(num_of_posts)
+        
+    return occurrences
     
 def main():
-    occurence = get_occurence() 
-    if len(occurence) > 0:  
-        fig = go.Figure([go.Bar(x=occurence[0][::-1], y=occurence[1][::-1])])
+    occurrences = get_occurrences() 
+    if len(occurrences) > 0:  
+        fig = go.Figure([go.Bar(x=occurrences[0][::-1], y=occurrences[1][::-1])])
         fig.show()
 
 if __name__=='__main__':
+    load_dotenv()
+    logging.basicConfig(level = logging.DEBUG, filename = u'log.txt') 
+    VK_SERVICE_KEY = os.getenv('VK_SERVICE_KEY')   
     main()
